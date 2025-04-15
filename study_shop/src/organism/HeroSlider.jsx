488d5bbe1feb9cf007bc46f6c6f3eb113e831a4e
H-JuKyung <hjk000hjk17@gmail.com>
2025-04-15 22:11:07 +0900

    feat: Header, Banner 기능 구현

---
+++ study_shop/src/organism/HeroSlider.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination } from 'swiper/modules'
+import css from './HeroSlider.module.css'
+import { Link } from 'react-router-dom'
+
+import { getBannerData } from '../api/bannerApi'
+
+const HeroSlider = () => {
+  const [banner, setBanner] = useState([])
+  useEffect(() => {
+    const fetchBanner = async () => {
+      try {
+        const data = await getBannerData()
+        console.log('data----', data)
+        setBanner(data)
+      } catch (err) {
+        console.log('err----', err)
+      }
+    }
+    fetchBanner()
+  }, [])
+
+  return (
+    <section>
+      <h2 hidden>Banner Event</h2>
+      <Swiper pagination={{ clickable: true }} modules={[Pagination]} className={css.mainSlider}>
+        {banner.map(item => (
+          <SwiperSlide key={item.id}>
+            <div className={css.imgWrap}>
+              <img src={item.img} alt={item.title} />
+            </div>
+            <div className={css.textWrap}>
+              <p className={css.title}>{item.title}</p>
+              <p className={css.desc}>{item.description}</p>
+              <Link to={item.link} className={css.more}>
+                View Product
+              </Link>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  )
+}
+
+export default HeroSlider
